847d77b982abd22ce97aca727dbccf611c7dc8bc
Tessa Lau <tlau@savioke.com>
2014-07-08 14:03:45 -0700

    Enables sending messages from the server too, not just the client

--- messages.js
+++ messages.js
@@ -1,14 +1,29 @@
-// Local (client-only) collection
-messages = new Meteor.Collection(null);
+// This file is shared between server and client
+
+// Collection shared between client and server
+messages = new Meteor.Collection("messages");
 
 Messages = {
 
-	send: function(style, message) {
-		messages.insert({ style: style, message: message, seen: false });
-	},
+  send: function(style, message) {
+    console.log("Logging message:", message);
+    Meteor.call("messages_insert", style, message);
+  },
 
-	clear: function() {
-		messages.remove({ seen: true });
-	}
-	
+  clear: function() {
+    console.log("Clearing messages");
+    Meteor.call("messages_clear");
+  }
+  
 }
+
+Meteor.methods({
+  messages_clear: function() {
+    console.log("Clearing messages");
+    messages.remove({seen: true}); 
+  },
+
+  messages_insert: function(style, message) {
+    messages.insert({ style: style, message: message, seen: false });
+  }
+});
--- package.js
+++ package.js
@@ -1,18 +1,21 @@
 Package.describe({
-	summary: "A package to display flash messages to the user with Foundation CSS styling."
+  summary: "A package to display flash messages to the user with Foundation CSS styling."
 });
 
 Package.on_use(function(api, where) {
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
-	api.add_files(['messages.js', 'messages_list.html', 'messages_list.js'], 'client');
-	
-	if (typeof api.export !== 'undefined') {
-    api.export(['Messages', 'messages'], 'client');
+  api.use(['minimongo', 'mongo-livedata'], 'server');
+  api.add_files(['messages-client.js', 'messages_list.html', 'messages_list.js'], 'client');
+  api.add_files(['messages.js'], ['client', 'server']);
+  api.add_files(['messages-server.js'], 'server');
+  
+  if (typeof api.export !== 'undefined') {
+    api.export(['Messages', 'messages'], ['client', 'server']);
   }
-	
+  
 });
 
 Package.on_test(function(api) {
-  	api.use(['foundation-flash-messages','tinytest', 'test-helpers'], 'client');  
-  	api.add_files('messages_tests.js', 'client');
+    api.use(['foundation-flash-messages','tinytest', 'test-helpers'], 'client');  
+    api.add_files('messages_tests.js', 'client');
 });
